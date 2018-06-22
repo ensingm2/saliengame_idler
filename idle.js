1fe8c3bcbb7d08e1ab8d6b27b93fabe899e81a40
Michael Ensing <ensingm2@gmail.com>
2018-06-22 19:02:10 -0400

    Changed update frequency to 1s
    It actually kind of matters now that output is in the GUI. This should
    likely be handled better than it is (smaller setInterval to update the
    timer, larger one that just waits until the end of round and then ends).

--- idle.js
+++ idle.js
@@ -7,7 +7,7 @@ var target_zone = -1;
 // Variables. Don't change these unless you know what you're doing.
 var max_scores = [600, 1200, 2400] // Max scores for each difficulty (easy, medium, hard)
 var round_length = 120; // Round Length (In Seconds)
-var update_length = 15; // How long to wait between updates (In Seconds)
+var update_length = 1; // How long to wait between updates (In Seconds)
 var loop_rounds = true;
 var language = "english"; // Used when POSTing scores
 var access_token = "";
