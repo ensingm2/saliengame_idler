63294be5ead7d00a513c88f245c7a8861e7864ab
Harest <Harest@users.noreply.github.com>
2018-06-23 19:51:48 +0200

    Fix error manag. for current planet while fetching
    Avoid the case where the current planet is captured and activePlanetsScore[current_planet_id] would be undefined. So we're sure the score = 0 would be a network error.

--- idle.js
+++ idle.js
@@ -447,7 +447,7 @@ function GetBestPlanet() {
 			success: function(data) {
 				data.response.planets[0].zones.forEach( function ( zone ) {
 					if (zone.difficulty >= 1 && zone.difficulty <= 3 && zone.captured == false)
-						activePlanetsScore[planet_id] += Math.round(auto_switch_planet["coeffScore"][zone.difficulty] * (1 - zone.capture_progress));
+						activePlanetsScore[planet_id] += Math.ceil(auto_switch_planet["coeffScore"][zone.difficulty] * (1 - zone.capture_progress));
 				});
 			},
 			error: function() {
@@ -461,7 +461,7 @@ function GetBestPlanet() {
 	});
 	console.log(activePlanetsScore);
 	// Prevent a planet switch if there were >= 2 errors while fetching planets or if there's an error while fetching the current planet score
-	if (numberErrors >= 2 || activePlanetsScore[current_planet_id] == 0)
+	if (numberErrors >= 2 || ((current_planet_id in activePlanetsScore) && activePlanetsScore[current_planet_id] == 0))
 		return null;
 	
 	return bestPlanetId;
