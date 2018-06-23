d1082a0de05ffc326ab93de5e3d337c7a7728e56
Jap2-0 <40511669+Jap2-0@users.noreply.github.com>
2018-06-23 17:54:09 -0400

    First attempt to make Lvl Up In real-time

--- idle.js
+++ idle.js
@@ -14,6 +14,7 @@ var max_retry = 5; // Max number of retries to send requests
 var auto_first_join = true; // Automatically join the best zone at first
 var current_planet_id = undefined;
 var current_game_start = undefined; // Timestamp for when the current game started
+var time_passed_ms = undefined
 
 class BotGUI {
 	constructor(state) {
@@ -98,12 +99,13 @@ var gui = new BotGUI({
 
 function calculateTimeToNextLevel() {	
 	const nextScoreAmount = get_max_score(target_zone);
-	const missingExp = Math.ceil((gPlayerInfo.next_level_score - gPlayerInfo.score) / nextScoreAmount) * nextScoreAmount;
+	if(time_passed_ms === undefined)
+		time_passed_ms = 0;
+	const missingExp = Math.ceil((gPlayerInfo.next_level_score - gPlayerInfo.score) / nextScoreAmount) * nextScoreAmount - time_passed_ms / 1000;
 	const roundTime = resend_frequency + update_length;
-
 	const secondsLeft = missingExp / nextScoreAmount * roundTime;
 
-	return secondsLeft;
+	gui.updateEstimatedTime(secondsLeft);
 }
 
 // Grab the user's access token
@@ -164,7 +166,7 @@ var INJECT_start_round = function(zone, access_token, attempt_no) {
 				// Update the GUI
 				gui.updateStatus(true);
 				gui.updateZone(zone, data.response.zone_info.capture_progress, data.response.zone_info.difficulty);
-				gui.updateEstimatedTime(calculateTimeToNextLevel())
+				calculateTimeToNextLevel()
 
 				current_game_id = data.response.zone_info.gameid;
 				current_game_start = new Date().getTime();
@@ -180,11 +182,12 @@ var INJECT_start_round = function(zone, access_token, attempt_no) {
 // Update time remaining, and wait for the round to complete.
 var INJECT_wait_for_end = function() {
 	var now = new Date().getTime();
-	var time_passed_ms = now - current_game_start;
+	time_passed_ms = now - current_game_start;
 	var time_remaining_ms = (resend_frequency*1000) - time_passed_ms;
 	var time_remaining = Math.round(time_remaining_ms/1000);
 	gui.updateTask("Waiting " + time_remaining + "s for round to end", false);
-
+	calculateTimeToNextLevel()
+	
 	// Wait
 	var wait_time = update_length*1000;;
 	var callback;
@@ -238,7 +241,7 @@ var INJECT_end_round = function(attempt_no) {
 				gui.updateLevel(data.response.new_level);
 				gui.updateExp(data.response.new_score);
 				// When we get a new EXP we also want to recalculate the time for next level.
-				gui.updateEstimatedTime(calculateTimeToNextLevel())
+				calculateTimeToNextLevel()
 
 				// Update the player info in the UI
 				INJECT_update_player_info();
